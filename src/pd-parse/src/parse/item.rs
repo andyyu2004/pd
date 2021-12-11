5f61cfa9d5b04aaa3cf7a345b14f12b2c4303495
Andy Yu <andyyu2004@gmail.com>
2021-12-11 15:08:18 +1300

    change value binding syntax to be `let <pat> = <expr>` rather than `let <ident> = <expr>`

--- src/pd-parse/src/parse/item.rs
+++ src/pd-parse/src/parse/item.rs
@@ -22,7 +22,7 @@ pub(crate) fn parse_item(p: &mut Parser<'_>) {
 pub(crate) fn parse_value_def(p: &mut Parser<'_>) {
     p.enter(T![ValueDef], |p| {
         p.bump(T![let]);
-        p.expect(T![Ident]);
+        parse_pat(p);
         if p.accept(T![:]) {
             parse_type(p);
         }
--- src/pd-parse/src/parse/tests/item.rs
+++ src/pd-parse/src/parse/tests/item.rs
@@ -1,3 +1,5 @@
+use std::arch::x86_64::_MM_EXCEPT_INEXACT;
+
 use expect_test::expect;
 use pd_lex::Span;
 use pd_syntax::{ast, T};
@@ -8,53 +10,62 @@ use crate::{parse, parse_syntax};
 #[test]
 fn test_parse_item_value_def() {
     let syntax = parse_syntax!(ast::Item, let x = false);
-    let expected = r#"ValueDef@0..13
-  LetKw@0..3 "let"
-  Whitespace@3..4 " "
-  Ident@4..5 "x"
-  Whitespace@5..6 " "
-  Equal@6..7 "="
-  Whitespace@7..8 " "
-  Literal@8..13
-    FalseKw@8..13 "false"
-"#;
-    assert_eq!(format!("{:#?}", syntax), expected);
+    let expected = expect![[r#"
+        ValueDef@0..13
+          LetKw@0..3 "let"
+          Whitespace@3..4 " "
+          Binding@4..6
+            Ident@4..5 "x"
+            Whitespace@5..6 " "
+          Equal@6..7 "="
+          Whitespace@7..8 " "
+          Literal@8..13
+            FalseKw@8..13 "false"
+
+    "#]];
+    expected.assert_debug_eq(&syntax)
 }
 
 #[test]
 fn test_parse_item_value_def_with_ty() {
-    let parsed = parse::<ast::Item>(stringify!(let x: bool = false));
-    let expected = r#"ValueDef@0..20
-  LetKw@0..3 "let"
-  Whitespace@3..4 " "
-  Ident@4..5 "x"
-  Whitespace@5..6 " "
-  Colon@6..7 ":"
-  Whitespace@7..8 " "
-  Type@8..13
-    Path@8..13
-      Ident@8..12 "bool"
-      Whitespace@12..13 " "
-  Equal@13..14 "="
-  Whitespace@14..15 " "
-  Literal@15..20
-    FalseKw@15..20 "false"
-"#;
-    assert_eq!(format!("{:#?}", parsed.syntax()), expected);
+    let syntax = parse_syntax!(ast::Item, let x: bool = false);
+    let expected = expect![[r#"
+        ValueDef@0..20
+          LetKw@0..3 "let"
+          Whitespace@3..4 " "
+          Binding@4..6
+            Ident@4..5 "x"
+            Whitespace@5..6 " "
+          Colon@6..7 ":"
+          Whitespace@7..8 " "
+          Type@8..13
+            Path@8..13
+              Ident@8..12 "bool"
+              Whitespace@12..13 " "
+          Equal@13..14 "="
+          Whitespace@14..15 " "
+          Literal@15..20
+            FalseKw@15..20 "false"
+
+    "#]];
+    expected.assert_debug_eq(&syntax);
 }
 
 #[test]
 fn test_parse_item_value_def_missing_equal() {
     let parsed = parse::<ast::Item>(stringify!(let x false));
-    let expected = r#"ValueDef@0..11
-  LetKw@0..3 "let"
-  Whitespace@3..4 " "
-  Ident@4..5 "x"
-  Whitespace@5..6 " "
-  Literal@6..11
-    FalseKw@6..11 "false"
-"#;
-    assert_eq!(format!("{:#?}", parsed.syntax()), expected);
+    let expected = expect![[r#"
+        ValueDef@0..11
+          LetKw@0..3 "let"
+          Whitespace@3..4 " "
+          Binding@4..6
+            Ident@4..5 "x"
+            Whitespace@5..6 " "
+          Literal@6..11
+            FalseKw@6..11 "false"
+
+    "#]];
+    expected.assert_debug_eq(&parsed.syntax());
 }
 
 #[test]
